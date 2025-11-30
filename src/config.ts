f2ec1f1e48eb456951c76d3a1b4199532e658dda
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-12-01 03:12:56 +0800

    merge changes from silverster

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const backendPrefix = 'https://viralmotion-backend-ghxi.onrender.com';
+export const backendPrefix = 'http://localhost:3000';
 export const remotion_lambda_serve_url = "https://remotionlambda-useast1-ov4ef5yc3h.s3.us-east-1.amazonaws.com/sites/viral-motion/index.html";
 export const remotion_lambda_site_name = "viral-motion";
