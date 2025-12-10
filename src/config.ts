37c1351e610cb1ef2577eb45478b098c37b182d3
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-12-10 15:26:34 +0800

    changed backend provider

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const backendPrefix = 'https://viralmotion-backend-ghxi.onrender.com';
+export const backendPrefix = 'https://viralmotion-backend-kx15.onrender.com';
 export const remotion_lambda_serve_url = "https://remotionlambda-useast1-ov4ef5yc3h.s3.us-east-1.amazonaws.com/sites/viral-motion/index.html";
 export const remotion_lambda_site_name = "viral-motion";
 
